2642032b5d0545756fd8256c4b9666d13b99b621
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-02-05 13:33:13 +0200

    add intersection

--- index.ts
+++ index.ts
@@ -39,3 +39,52 @@ const sendInvoice = ([electricity, water]: number[], {readings, units}:{readings
 
 const invoice = sendInvoice(monthPayments, electricityUserData, waterUserData);
 console.log(invoice);
+
+const num = (arg: string | number): string | number => {
+   if (typeof arg === 'string') {
+    return 'hello'
+   } else {
+     return 'world'
+   }
+}
+console.log(num(9))
+
+const port3000: number = 3000;
+const port3001: number = 3001;
+
+let msg: 'Hello' = 'Hello'
+msg = 'Hello';
+
+function startServer(protocol:'http' | 'https', port: 3000 | 3001): 'Server started' {
+  if (port === port3000 || port === port3001) {
+    console.log(`Server started on ${protocol}://server: ${port}`);
+  } else {
+    console.log('Invalid port')
+  }
+  return 'Server started';
+}
+startServer('https', 3001);
+
+type Timing = 'ease' | 'ease-out' | 'ease-in';
+type AnimationId = string | number;
+
+function createAnimation(id: AnimationId, animName: string, timingFunc: Timing = 'ease', duration: number, iterCount: 'infinite' | number ): void {
+/*   const elem = document.querySelector(`#${id}`) as HTMLElement
+  if (elem) {
+    elem.style.animation = `${animName} ${timingFunc} ${duration} ${iterCount}`
+  } */ 
+  console.log(`${animName} ${timingFunc} ${duration} ${iterCount}`)
+}
+createAnimation('id', 'fade', 'ease', 5, 'infinite')
+/* intersection */
+const obj1 = {
+  exam: 'fff',
+  exam2: 'ggg'
+}
+const obj2 = {
+  name: 'jjj'
+}
+type First = {exam: string | number; exam2: string | number}
+type Second = {name: string | number}
+
+type FirstAndSecond = First & Second;
