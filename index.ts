8eb1580fc146df3cc089eb9147418c24fe6b5ce8
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-02-13 00:09:03 +0200

    add home work 2

--- index.ts
+++ index.ts
@@ -19,3 +19,59 @@ function transferEurToUsd(available: boolean, amount: number, commission: number
 
 transferEurToUsd(true, 500, 1.05);
 
+/* Home Work 2 */
+
+const electricityUserData = {
+	readings: 95,
+	units: "kWt",
+	mode: "double",
+};
+
+const waterUserData = {
+	readings: 3,
+	units: "m3",
+};
+
+const elRate: number = 0.45;
+const wRate: number = 2;
+
+const monthPayments: number[] = [0, 0]; // [electricity, water]
+
+const calculatePayments = ({readings, units, mode}:{
+  readings: number,
+	units: string,
+	mode: string,}, 
+  wData:{
+  readings: number,
+  units: string,
+  }, elRate: number, wRate: number): void => {
+	if (mode === "double" && readings < 50) {
+		monthPayments[0] = readings * elRate * 0.7;
+	} else {
+		monthPayments[0] = readings * elRate;
+	}
+
+	monthPayments[1] = wData.readings * wRate;
+};
+
+calculatePayments(electricityUserData, waterUserData, elRate, wRate);
+
+const sendInvoice = ([el, water]: number[], electricityUserData: {
+  readings: number,
+	units: string,
+	mode: string,},
+  waterUserData:{
+  readings: number,
+  units: string,
+  } ):string => {
+	const text = `    Hello!
+    This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
+    It will cost: ${el}€
+    
+    This month you used ${waterUserData.readings} ${waterUserData.units} of water
+    It will cost: ${water}€`;
+
+	return text;
+};
+
+console.log(sendInvoice(monthPayments, electricityUserData, waterUserData))
