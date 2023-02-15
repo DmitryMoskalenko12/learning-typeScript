5f652e4369ef742d21ea827e2baa3714600344ad
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-02-15 14:36:15 +0200

    add home work 8

--- index.ts
+++ index.ts
@@ -388,3 +388,185 @@ const data: WithData[] = [
 ];
 
 console.log(calculateAmountOfFigures<Data>(data));
+
+/* Home work 7 */
+
+interface IPhone {
+	company: string;
+	number: number;
+}
+
+// IMobilePhone должен наследоваться от IPhone,
+// тип свойства companyPartner зависит от свойства company
+
+interface IMobilePhone extends IPhone {
+	size: string;
+	companyPartner: IPhone['company'];
+	manufactured: Date;
+}
+
+// Типизировать объект phones
+
+const phones: IMobilePhone[] = [
+	{
+		company: "Nokia",
+		number: 1285637,
+		size: "5.5",
+		companyPartner: "MobileNokia",
+		manufactured: new Date("2022-09-01"),
+	},
+	{
+		company: "Samsung",
+		number: 4356637,
+		size: "5.0",
+		companyPartner: "SamMobile",
+		manufactured: new Date("2021-11-05"),
+	},
+	{
+		company: "Apple",
+		number: 4552833,
+		size: "5.7",
+		companyPartner: "no data",
+		manufactured: new Date("2022-05-24T12:00:00"),
+	},
+];
+
+interface IPhonesManufacturedAfterDate extends IMobilePhone {
+	initialDate: string;
+}
+
+// Функция должна отфильтровать массив данных и вернуть новый массив
+// с телефонами, выпущенными после даты в третьем аргументе
+
+function filterPhonesByDate(phones: IMobilePhone[], key: keyof IMobilePhone, initial: string): IPhonesManufacturedAfterDate[] {
+ return phones.filter(item => {
+    if (Date.parse(item[key].toString()) > Date.parse(initial)) {
+      return item
+    }
+  }).map(phone => {
+    return{
+    ...phone,
+    initialDate: initial
+    }
+  })
+}
+
+// Второй аргумент при вызове функции должен быть связан с первым,
+// а значит мы получим подсказки - свойства этого объекта
+
+console.log(filterPhonesByDate(phones, 'manufactured', "2022-01-01"));
+
+/* Home work 8 */
+
+// Необходимо типизировать этот большой объект
+// Свойство futureClasses должно быть в зависимости от classes по типу
+// Свойства exClients и futureClients тоже должны быть в зависимости от currClients
+// ИЛИ все три зависят от общего родителя
+
+// Простыми словами: при добавлении свойства в целевой объект они должны быть
+// автоматически добавлены в зависимые (сразу подсказка от TS)
+
+interface Classes {
+  name: string,
+  startsAt: string,
+  duration: number,
+}
+interface CurrClients {
+	name: string,
+  age: string | number,
+  gender: string,
+  timeLeft: string,
+}
+type WithOutStartsAt = Omit<Classes, 'startsAt'>;
+type ExClients = Omit<CurrClients, 'timeLeft'>;
+type FutureClients = Pick<CurrClients, 'name'>;
+
+interface FutureClient extends FutureClients {
+  makeCallFor: Date
+}
+
+interface ExClient extends ExClients {
+  makeCallFor: Date;
+}
+
+interface FutureClasses extends WithOutStartsAt {
+  willStartsAt: string
+}
+
+interface FitnessClubCenter {
+  clubName: string;
+  location: string;
+  classes: Classes[];
+  futureClasses: FutureClasses[];
+  currClients: CurrClients[];
+  exClients: ExClient[];
+  futureClients: FutureClient[]
+}
+const fitnessClubCenter: FitnessClubCenter = {
+	clubName: "Fitness club Center",
+	location: "central ave. 45, 5th floor",
+	classes: [
+		{
+			name: "yoga",
+			startsAt: "8:00 AM",
+			duration: 60,
+		},
+		{
+			name: "trx",
+			startsAt: "11:00 AM",
+			duration: 45,
+		},
+		{
+			name: "swimming",
+			startsAt: "3:00 PM",
+			duration: 70,
+		},
+	],
+	futureClasses: [
+		{
+			name: "boxing",
+			willStartsAt: "6:00 PM",
+			duration: 40,
+		},
+		{
+			name: "breath training",
+			willStartsAt: "8:00 PM",
+			duration: 30,
+		},
+	],
+	currClients: [
+		{
+			name: "John Smith",
+			age: "-",
+			gender: "male",
+			timeLeft: "1 month",
+		},
+		{
+			name: "Alise Smith",
+			age: 35,
+			gender: "female",
+			timeLeft: "3 month",
+		},
+		{
+			name: "Ann Sonne",
+			age: 24,
+			gender: "female",
+			timeLeft: "5 month",
+		},
+	],
+	exClients: [
+		{
+			name: "Tom Smooth",
+			age: 50,
+			gender: "male",
+			makeCallFor: new Date("2023-08-12"),
+		},
+	],
+	futureClients: [
+		{
+			name: "Maria",
+			makeCallFor: new Date("2023-07-10"),
+		},
+	],
+};
+
